93c6e4c9bf1a31a8b6ea50336d8fbc7f22a02b1f
fernando <413114463@qq.com>
2025-12-26 15:20:25 +0800

    demo1增加用例

--- assemblyScriptDemo/assembly/index.ts
+++ assemblyScriptDemo/assembly/index.ts
@@ -1,3 +1,11 @@
 export function add(a: number, b: number): number {
     return a+b;
 }
+
+export function sum(a: number, b: number): number {
+    let res = 0;
+    for (let i =0;i<1000000;i++) {
+        res+=i;
+    }
+    return a+b;
+}
